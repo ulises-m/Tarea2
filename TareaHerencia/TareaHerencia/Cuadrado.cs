d2445273992f0c9dff32d929bc4e11d5212e9ca4
ulises-m <ulises_cruz_19@outlook.com>
2013-12-09 00:56:07 -0600

    Initial commit

---
+++ TareaHerencia/TareaHerencia/Cuadrado.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace TareaHerencia
+{
+
+	public class Cuadrado:Figura//Herencia de la clase figura
+	{
+			private double _lado;//variable global _lado
+		public Cuadrado( double lado )
+		{
+		
+			this._lado = lado; 
+		
+		}
+		
+		public override void Dibujar()//se sobreescribe el metodo Dibujar de la clase base Figura
+		{
+			Console.WriteLine("Dibujando Cuadrado");
+		}
+		
+		public override double Area()
+		{
+			return  Math.Pow(this._lado,2);//Area del cuadrado funciones de la libreria Math
+		}
+	}
+}
